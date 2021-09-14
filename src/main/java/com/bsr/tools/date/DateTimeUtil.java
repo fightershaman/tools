6dbb1ebc05a90fef4d9f092a892dcc172c789d60
zhf <81082615@qq.com>
2021-09-14 14:08:29 +0800

    增加Date转LocalDateTie

--- src/main/java/com/bsr/tools/date/DateTimeUtil.java
+++ src/main/java/com/bsr/tools/date/DateTimeUtil.java
@@ -1,11 +1,22 @@
 package com.bsr.tools.date;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * 时间日期工具
  */
 public class DateTimeUtil {
+    /**
+     * 日期转换
+     *
+     * @param yearToAdd
+     * @param monthToAdd
+     * @param dayToAdd
+     * @return
+     */
     public static LocalDate dateChange(Long yearToAdd, Long monthToAdd, Long dayToAdd) {
         LocalDate ld = LocalDate.now();
         if (yearToAdd != null) {
@@ -18,5 +29,16 @@ public class DateTimeUtil {
             ld = ld.plusDays(dayToAdd);
         }
         return ld;
+    }
+
+    /**
+     * Date转换LocalDateTime
+     *
+     * @param date
+     * @return
+     */
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        Instant instant = date.toInstant();
+        return instant.atZone(ZoneUtil.systemZoneId()).toLocalDateTime();
     }
 }
