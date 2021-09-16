1f7f080a6f7c35beda1b21e3d7a85fc2275b78fe
zhf <81082615@qq.com>
2021-09-16 13:35:41 +0800

    增加Date工具类

---
+++ src/main/java/com/bsr/tools/date/DateUtil.java
@@ -0,0 +1,21 @@
+package com.bsr.tools.date;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * Date工具类
+ */
+public class DateUtil {
+    /**
+     * LocalDateTime转换Date
+     *
+     * @param ldt
+     * @return
+     */
+    public static Date localDateTimeToDate(LocalDateTime ldt) {
+        Instant instant = ldt.atZone(ZoneUtil.systemZoneId()).toInstant();
+        return Date.from(instant);
+    }
+}
