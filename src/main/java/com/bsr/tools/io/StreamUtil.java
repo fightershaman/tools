d8af213fb802525ec8e0c9caf7fda707b3ec2cad
zhf <81082615@qq.com>
2021-09-17 15:44:00 +0800

    增加输入流转换输出流方法

---
+++ src/main/java/com/bsr/tools/io/StreamUtil.java
@@ -0,0 +1,29 @@
+package com.bsr.tools.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 输入输出流工具
+ */
+public class StreamUtil {
+    /**
+     * 输入流转输出流
+     *
+     * @param inputStream
+     * @param outputStream
+     * @return
+     * @throws IOException
+     */
+    public static int inputToOutput(InputStream inputStream, OutputStream outputStream) throws IOException {
+        int i = 0;
+        int j = 0;
+        byte[] bytes = new byte[1024 * 8];
+        while ((j = inputStream.read(bytes)) > 0) {
+            outputStream.write(bytes);
+            i += j;
+        }
+        return i;
+    }
+}
