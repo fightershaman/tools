572e1b257d444e5348a8c66bb0acd85d2653d1a3
zhf <81082615@qq.com>
2021-09-23 14:51:34 +0800

    增加flush，适配带缓存的输出流

---
+++ src/main/java/com/bsr/tools/document/DirectoryUtil.java
@@ -0,0 +1,24 @@
+package com.bsr.tools.document;
+
+import java.io.File;
+import java.nio.file.Files;
+
+/**
+ * 文件夹工具类
+ */
+public class DirectoryUtil {
+    /**
+     * 文件夹是否存在，不存在则创建
+     *
+     * @param directoryPath 文件夹路径
+     * @return 文件夹是否存在
+     */
+    public static boolean existsOrCreate(String directoryPath) {
+        File directory = new File(directoryPath);
+        if (!directory.exists() || !directory.isDirectory()) {
+            return directory.mkdirs();
+        } else {
+            return true;
+        }
+    }
+}
