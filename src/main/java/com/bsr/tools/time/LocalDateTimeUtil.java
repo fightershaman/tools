761289438d55e3c59dad4ca85a4357472f38b595
zhf <81082615@qq.com>
2022-04-26 13:48:12 +0800

    增加utc时间转换localdatetime

--- src/main/java/com/bsr/tools/time/LocalDateTimeUtil.java
+++ src/main/java/com/bsr/tools/time/LocalDateTimeUtil.java
@@ -18,4 +18,15 @@ public class LocalDateTimeUtil {
         Instant instant = date.toInstant();
         return instant.atZone(ZoneUtil.systemZoneId()).toLocalDateTime();
     }
+
+    /**
+     * utc时间转换LocalDateTime
+     *
+     * @param utc
+     * @return
+     */
+    public static LocalDateTime utcToLocalDateTime(long utc) {
+        Instant instant = Instant.ofEpochMilli(utc);
+        return instant.atZone(ZoneUtil.systemZoneId()).toLocalDateTime();
+    }
 }
