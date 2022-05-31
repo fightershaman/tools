ea187ad41fce9670ec749fc06dd70522a4508be4
zhf <81082615@qq.com>
2022-05-31 13:49:59 +0800

    解压缩

---
+++ src/main/java/com/bsr/tools/compress/ZipUtil.java
@@ -0,0 +1,60 @@
+package com.bsr.tools.compress;
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class ZipUtil {
+    public static void main(String[] args) throws IOException {
+        //unzip("./avatar_imag.zip", "./");
+        zip("./","/home/zhf/zhf/tools/src");
+    }
+
+    public static void zip(String zipPath, String inPath) {
+        File inFile = new File(inPath);
+        if (inFile.isDirectory()) {
+            File[] files = inFile.listFiles();
+
+        } else {
+
+
+        }
+    }
+
+    public static void unzip(String zipPath, String outPath) throws IOException {
+        File file = new File(zipPath);
+        if (!file.exists()) {
+            throw new FileNotFoundException("zip file not found");
+        }
+        ZipFile zipFile = new ZipFile(file);
+        Enumeration entries = zipFile.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = (ZipEntry) entries.nextElement();
+            System.out.println(entry.getName());
+            if (entry.isDirectory()) {
+                //文件夹
+                File newFile = new File(outPath, entry.getName());
+                if (!newFile.exists()) {
+                    newFile.mkdirs();
+                }
+            } else {
+                //文件
+                File newFile = new File(outPath, entry.getName());
+                File parentFile = newFile.getParentFile();
+                if (!parentFile.exists()) {
+                    parentFile.mkdirs();
+                }
+                try (InputStream zis = zipFile.getInputStream(entry); FileOutputStream fos = new FileOutputStream(newFile)) {
+                    byte[] bytes = new byte[8 * 1024];
+                    int length = 0;
+                    while ((length = zis.read(bytes)) > 0) {
+                        fos.write(bytes, 0, length);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
